b18558975266a0daedc0f263211853b58498682e
reshalla <reshalla01@gmail.com>
2023-02-18 19:05:00 +0530

    first commit

---
+++ src/co/pack/selenium_introduction/BrowserActivities.java
@@ -0,0 +1,24 @@
+package co.pack.selenium_introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserActivities {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\areshma\\OneDrive - Capgemini\\Documents\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		System.out.println("Browser window maxmized");
+		driver.get("https:google.com/");
+		driver.navigate().to("https:rahulshettyacademy.com/");
+		driver.navigate().back();
+		System.out.println("Browser navigated back to google.com");
+		driver.navigate().forward();
+		System.out.println("Browser navigated forward to rahulshettyaceademy.com");
+
+	}
+
+}
---
+++ src/co/pack/selenium_introduction/seleniumBasic.java
@@ -0,0 +1,21 @@
+package co.pack.selenium_introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumBasic {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\areshma\\OneDrive - Capgemini\\Documents\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+
+	}
+
+}
